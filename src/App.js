cc5f94eb68ea7e890c31290d2a2f18312e0b6602
Yulian Borges Lobaina <yborges2005@gmail.com>
2024-11-17 14:45:33 +0100

    Modificados App.js y App.css para que muestren la home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import BannerAnuncios from './components/BannerAnuncios/BannerAnuncios';
+import Header from './components/Header/Header';
+import Tendencias from './components/Tendencias/Tendencias';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Header />
       </header>
+      <main>
+        <section>
+          <BannerAnuncios />
+        </section>
+        <section>
+          <Tendencias />
+        </section>
+      </main>
+      <footer>
+
+      </footer>
     </div>
+
   );
 }
 

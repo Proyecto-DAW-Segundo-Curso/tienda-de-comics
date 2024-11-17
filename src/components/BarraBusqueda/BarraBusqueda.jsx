604ca3bb925b6bd66d01acd3137ca08971bc35b8
Yulian Borges Lobaina <yborges2005@gmail.com>
2024-11-17 17:53:12 +0100

    Terminada la barra de búsqueda del header

--- src/components/BarraBusqueda/BarraBusqueda.jsx
+++ src/components/BarraBusqueda/BarraBusqueda.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './BarraBusqueda.css'
+import { FaSistrix } from "react-icons/fa6";
+import { IoMdMenu } from "react-icons/io";
 
 function BarraBusqueda() {
   return (
     <div>
       <div className='contenedor-busqueda'>
-        <div>H</div>
+        <div className='desplegable'>
+          <IoMdMenu />
+        </div>
         <input type='text' placeholder='buscar...' />
-        <div>B</div>
+        <div className='icono-busqueda'>
+          <FaSistrix />
+        </div>
 
       </div>
     </div>

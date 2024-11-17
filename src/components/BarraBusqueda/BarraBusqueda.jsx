2882fb4a10684bbcb64f9ab1e85911b92e41b0da
Yulian Borges Lobaina <yborges2005@gmail.com>
2024-11-17 15:34:21 +0100

    Retocada la barra de búsqueda

--- src/components/BarraBusqueda/BarraBusqueda.jsx
+++ src/components/BarraBusqueda/BarraBusqueda.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import './BarraBusqueda.css'
 
 function BarraBusqueda() {
   return (
     <div>
-        <div className='contenedor-busqueda'>
-        <input type='text' />
-        <div></div>
+      <div className='contenedor-busqueda'>
+        <div>H</div>
+        <input type='text' placeholder='buscar...' />
+        <div>B</div>
 
-        </div>
+      </div>
     </div>
   )
 }

b97c121d5c6f8396496c0af5d58bbc63a3364877
Pepe Templado <josetempladomena@gmail.com>
2024-11-29 20:32:51 +0100

    base-funcional

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import FormLogin from './components/FormLogin/FormLogin';
+import Intercambio from './components/Intercambio/Intercambio';
+
 
 function App() {
 
@@ -14,6 +16,25 @@ function App() {
     setComponenteMostrado(componete)
   }
 
+  // Simulamos datos de ofertas
+  const ofertas = [
+    {
+      titulo: 'Spiderman',
+      descripcion: 'Primera edición en buen estado',
+      precio: 50,
+      vendedor: 'Juan',
+      imagen: 'Spiderman_Marvel.png',  // URL de la imagen
+    },
+    {
+      titulo: 'Weekly Shonen Jump Nº 6',
+      descripcion: 'Nuevo con portada exclusiva',
+      precio: 20,
+      vendedor: 'Ana',
+      imagen: 'WeeklyShonenJumpNo6',  // URL de la imagen
+    },
+  ];
+  
+
   return (
     <div className="App">
       <header>
@@ -21,7 +42,12 @@ function App() {
       </header>
       <main>
         {componenteMostrado === "home" && <Home />}
+        
         {componenteMostrado === "login" && <FormLogin />}
+
+        {componenteMostrado === "intercambio" && <Intercambio ofertas={ofertas} />}
+        
+        
       </main>
       <footer>
         <Footer />

d5a4af5d08147ae0e50ca1a1932338b99aee023c
Yulian Borges Lobaina <yborges2005@gmail.com>
2024-12-01 16:57:05 +0100

    Creado json con info de comics. Modificado componente Home para que cargue y muestre la información del json

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import BannerAnuncios from '../BannerAnuncios/BannerAnuncios';
-import Tendencias from "../Tendencias/Tendencias1";
+import Tendencias from "../Tendencias/Tendencias";
+import Comics from "../../data/comics.json";
 
 function Home() {
+  console.log("hola");
+  
   return (
     <div>
       <section>
         <BannerAnuncios />
       </section>
       <section>
-        <Tendencias />
+        <Tendencias comics= {Comics} />
       </section>
 
     </div>

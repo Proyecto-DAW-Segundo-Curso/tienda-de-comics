f3f6f545c2d59563b6a8a002aa422638b52beeb5
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-02-10 09:13:59 +0100

    Añadido estilo personalizado al componente ZonaUsuario, mejorando el encabezado, el cuerpo y los botones, y reorganizada la estructura para una mejor presentación.

--- src/components/ZonaUsuario/ZonaUsuario.jsx
+++ src/components/ZonaUsuario/ZonaUsuario.jsx
@@ -16,40 +16,44 @@ const ZonaUsuario = ({ usuarioLogado }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="zona-usuario">
-      <div className="contenedor-titulo">
-        <h2>Zona de Usuario</h2>
-      </div>
-      {modoEdicion ? (
-        <FormularioUsuario
-          datosUsuario={usuario}
-          onGuardar={actualizarUsuario}
-          onCancelar={() => setModoEdicion(false)}
-        />
-      ) : (
-        <div className="datos-usuario">
-          <div className="tarjeta-info">
-            <p><strong>Nombre:</strong> {usuario.nombre}</p>
-            <p><strong>Email:</strong> {usuario.email}</p>
-            <p><strong>Contraseña:</strong> {usuario.contrasenia}</p>
-
-          </div>
-          <div className="tarjeta-opciones">
-            <button onClick={() => setModoEdicion(true)} className="btn">
-              Modificar Datos
-            </button>
-            <button onClick={() => navigate("/admin-comics")} className="btn">
-              Admin Comics
-            </button>
-            <button onClick={() => setModoEdicion(true)} className="btn">
-              Crear Oferta
-            </button>
-            <button onClick={() => setModoEdicion(true)} className="btn">
-              Mis Ofertas
-            </button>
-          </div>
+    <div className="container mt-5">
+      <div className="card">
+        <div className="card-header custom-header text-white fw-bold text-center">
+          ZONA DE USUARIO
         </div>
-      )}
+        <div className="card-body custom-body">
+          {modoEdicion ? (
+            <FormularioUsuario
+              datosUsuario={usuario}
+              onGuardar={actualizarUsuario}
+              onCancelar={() => setModoEdicion(false)}
+            />
+          ) : (
+            <div className="datos-usuario">
+              <div className="tarjeta-info">
+                <p><strong>Nombre:</strong> {usuario.nombre}</p>
+                <p><strong>Email:</strong> {usuario.email}</p>
+                <p><strong>Contraseña:</strong> {usuario.contrasenia}</p>
+
+              </div>
+              <div className="tarjeta-opciones">
+                <button onClick={() => setModoEdicion(true)} className="btn custom-button text-black fw-bold me-2">
+                  Modificar Datos
+                </button>
+                <button onClick={() => navigate("/admin-comics")} className="btn custom-button text-black fw-bold me-2">
+                  Admin Comics
+                </button>
+                <button onClick={() => setModoEdicion(true)} className="btn custom-button text-black fw-bold me-2">
+                  Crear Oferta
+                </button>
+                <button onClick={() => setModoEdicion(true)} className="btn custom-button text-black fw-bold me-2 text-black fw-bold me-2">
+                  Mis Ofertas
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
